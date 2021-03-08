fde7b8806e5cbf4eb701eb32cd59d936764a86c6
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-03-08 17:41:36 +0800

    Create 2021.03.08 实验一（2）.cpp

---
+++ 2021.03.08 实验一（2）.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main()
+{
+    int the_number = 0;
+    std::cout << "请输入一个整数" << std::endl;
+    std::cin >> the_number;
+    std::cout << "您输入的整数为" << the_number << std::endl;
+    return 0;
+}
